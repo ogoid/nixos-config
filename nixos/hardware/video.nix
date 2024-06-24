f296b491d306f9cc9759039176c265c0e137096c
ogoid <1449961+ogoid@users.noreply.github.com>
2024-06-24 15:35:57 -0300

    update flake

--- nixos/hardware/video.nix
+++ nixos/hardware/video.nix
@@ -5,10 +5,8 @@
   hardware = {
     # bumblebee.enable = true;
 
-    opengl = {
+    graphics = {
       enable = true;
-      driSupport = true;
-      # driSupport32Bit = true;
       extraPackages = with pkgs; [
         intel-media-driver
         vaapiIntel
