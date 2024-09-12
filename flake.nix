48ba75d7d3dcb96d5d744fbffd6c20b97718bd4d
ogoid <1449961+ogoid@users.noreply.github.com>
2024-09-12 16:38:56 -0300

    update flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     hardware.url = "github:nixos/nixos-hardware";
-    
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
