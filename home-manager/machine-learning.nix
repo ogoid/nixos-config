6b06b146a1aa184137da57fd232d46a70441daa6
ogoid <1449961+ogoid@users.noreply.github.com>
2024-09-12 16:38:44 -0300

    use pixi for machine learning

--- home-manager/machine-learning.nix
+++ home-manager/machine-learning.nix
@@ -1,7 +1,1 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    (python3.withPackages (p: with p; [ polars pandas matplotlib scikit-learn ]))
-    julia
- ];
-}
+{ pkgs, ... }: { home = { packages = with pkgs; [ pixi ]; }; }
