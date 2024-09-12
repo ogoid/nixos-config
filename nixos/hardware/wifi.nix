38554b6cb2481fa2a603a49177a9864712f1882c
ogoid <1449961+ogoid@users.noreply.github.com>
2024-09-12 16:29:11 -0300

    disable wifi fix

--- nixos/hardware/wifi.nix
+++ nixos/hardware/wifi.nix
@@ -1,7 +1,7 @@
 {
   # https://www.reddit.com/r/archlinux/comments/kuy8fq/wifi_not_working_iwlwifi_failed_with_error_110/
 
-  boot.extraModprobeConfig = ''
-  install iwlwifi echo 1 > /sys/bus/pci/devices/0000\:00\:14.3/reset ; /run/current-system/sw/bin/modprobe -i iwlwifi
-  '';
+  # boot.extraModprobeConfig = ''
+  # install iwlwifi echo 1 > /sys/bus/pci/devices/0000\:00\:14.3/reset ; /run/current-system/sw/bin/modprobe -i iwlwifi
+  # '';
 }
